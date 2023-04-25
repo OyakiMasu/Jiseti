aedec86af3fdeba5ae41a49a618beb6274896c10
ittstevin <tevingichoya@gmail.com>
2023-04-25 11:47:03 +0300

    updated

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = ({ setIsAdding }) => {
   return (
     <header>
+        <h1>Jiseti.</h1>
       <div style={{ marginTop: '30px', marginBottom: '18px' }}>
         <button onClick={() => setIsAdding(true)}>Add case</button>
       </div>
